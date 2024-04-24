2d7cd9baae1507f1e1cdf8301660fa111d17b1df
yaadevmohit <mohityadav530@gmail.com>
2024-04-24 14:01:10 -0400

    color change

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import "./about.css"
-import profileImg from "../../public/mohit.png"
+import profileImg from "../assets/photo_gallery/mohit.png"
 const About = () => {
     return (
         <>
@@ -11,7 +11,7 @@ const About = () => {
                     <hr />
                 </h1>
                 <p className="about-text">
-                    Experienced software developer proficient in React, Node.js, and Python frameworks.
+                    Experienced software developer proficient in React, Node.js, and other front-end frameworks.
                     Skilled in building user-friendly front-end interfaces with React and developing scalable
                     server-side applications. Strong problem-solving abilities and a commitment to clean,
                     maintainable code. Collaborative team player with excellent communication skills.
