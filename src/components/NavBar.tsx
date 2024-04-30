393f83a92cdf287f11824ca6ff8dd04af7dc72dc
yaadevmohit <mohityadav530@gmail.com>
2024-04-29 23:11:49 -0400

    add toggler

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,19 @@
 import "./nav-bar.css"
 import { Link } from "react-router-dom"
+import lightModeSvg from "../assets/photo_gallery/sun-svgrepo-com.svg"
+import darkModeSvg from  "../assets/photo_gallery/crescent-moon-svgrepo-com.svg"
+
 const NavBar = () => {
     return (
         <header>
             <div className="profile-header">
-                <h2>MOHIT YADAV</h2>
+                <h2 className="main-heading">MOHIT YADAV</h2>
+                <div className="toggler">
+                    <div className="toggle-btn"><img src={lightModeSvg} alt="" /></div>
+                    <p>Light mode</p>
+                </div>
+            </div>
+            <div>
             </div>
             <nav>
                 <Link className="nav-item" to="/projects">projects</ Link>
