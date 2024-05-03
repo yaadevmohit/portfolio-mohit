cc62ba545e07e3736b4705c5929597f62f49f73f
yaadevmohit <mohityadav530@gmail.com>
2024-05-03 01:40:17 -0400

    projects reduced

--- src/assets/projectData.tsx
+++ src/assets/projectData.tsx
@@ -1,8 +1,5 @@
 import libraryImg from "./projects_img/pr-1.png"
 import formImg from "./projects_img/pr-2.png"
-import dashBoardImg from "./projects_img/dashboard.png"
-import hangmanImg from "./projects_img/hangman-preview.gif"
-import ageCalcImg from "./projects_img/age-calc.png"
 export default 
     [
         {
@@ -20,29 +17,5 @@ export default
             skills: ["React", "JavaScript", "HTML", "CSS"],
             github: "https://github.com/yaadevmohit/modern-form",
             live: "https://multi-step-form-mohit.vercel.app/"
-        },
-        {
-            name: "Hangman",
-            img: hangmanImg,
-            info: "Classic hangman game with a few letters already guessed to make game slightly easier. ",
-            skills: ["React", "TypeScript", "HTML", "CSS"],
-            github: "https://github.com/yaadevmohit/hangman",
-            live: ""
-        },
-        {
-            name: "Dashboard",
-            img: dashBoardImg,
-            info: "Frontendmentor challenge. Uses CSS grid for the layout and avaible with dark and light theme.",
-            skills: ["HTML", "CSS"],
-            github: "https://github.com/yaadevmohit/dashboard-fem",
-            live: "https://dashboard-fem.vercel.app/",
-        },
-        {
-            name: "Age Calculator",
-            img: ageCalcImg,
-            info: "Calculates age based on the enetered input. Used CSS position property and flexbox for the layout. Error handling not implemented yet.",
-            skills: ["HTML", "CSS", "JavaScript"],
-            github: "https://github.com/yaadevmohit/age-calculator",
-            live: ""
-        }    
+        }
     ]
