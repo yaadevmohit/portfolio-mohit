53ea7b632c75846e3f460d8fdf45bc6f1db99221
yaadevmohit <mohityadav530@gmail.com>
2025-02-27 00:55:10 -0500

    new thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
 import './App.css'
-import { Routes, Route } from "react-router-dom"
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import Projects from './components/Projects'
-import About from './components/About'
-import { useState } from 'react'
-import Animations from './components/Animations'
+// import { Routes, Route } from "react-router-dom"
+// import NavBar from './components/NavBar'
+// import Footer from './components/Footer'
+// import Projects from './components/Projects'
+// import About from './components/About'
+// import { useState } from 'react'
+// import Animations from './components/Animations'
 
 function App() {
 
-  const [darkMode, setDarkMode] = useState(false);
+  // const [darkMode, setDarkMode] = useState(false);
 
-  function toggleDarkMode() {
-    setDarkMode(prevMode => !prevMode)
-  }
-  {document.body.style.backgroundColor = darkMode ? "#1A120B" : "white"}
+  // function toggleDarkMode() {
+  //   setDarkMode(prevMode => !prevMode)
+  // }
+  // {document.body.style.backgroundColor = darkMode ? "#1A120B" : "white"}
   return (
     <>
-      <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
+      {/* <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode}/> */}
       <div className='main-container'>
-        <Routes>
+        {/* <Routes>
           <Route path='/' element={<About darkMode={darkMode}/>} />
           <Route path='/projects' element={<Projects darkMode={darkMode}/>}/>
           <Route path="/animations" element={<Animations />}/>
-        </Routes>
+        </Routes> */}
+        <h1>#ballin 😎</h1>
+        <span>big things coming up god (bill gates) is kind</span>
       </div>
-      <Footer darkMode={darkMode}/>
+      {/* <Footer darkMode={darkMode}/> */}
     </>
   )
 }
